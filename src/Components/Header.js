6d832b30b5fb01ba06fa296e444cb8553b3903f4
gedunlap <g.e.dunlap90@gmail.com>
2021-07-19 16:27:39 -0400

    nav mod

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,14 +3,17 @@ import { Link } from "react-router-dom"
 function Header(props){
     return (
       <div className="nav">
-        <Link to="/" className="navitem">
-          <div>Home</div>
-        </Link>
         <Link to="/projects" className="navitem">
           <div>Projects</div>
         </Link>
         <Link to="/about" className="navitem">
           <div>About Me</div>
+        </Link>
+        <Link to="/" className="navitem">
+          <div>&lt;GD&gt;</div>
+        </Link>
+        <Link to="/contact" className="navitem">
+          <div>Contact</div>
         </Link>
         <a href="https://www.linkedin.com/in/garrett-dunlap-4b584a209/" className="navitem">
           <div>LinkedIn</div>
---
+++ src/Pages/Contact.js
@@ -0,0 +1,23 @@
+function About(props){
+    return (
+      <div className="container about">
+      <div className="row aboutstuff">
+          <img className="six columns aboutimg" src="https://i.imgur.com/4xMArG3.jpg" alt="" />
+          <div className="six columns aboutinfo">
+              <h2>About Me</h2>
+              <h4>I am a husband and dog dad with a background in project management, account management and custom sheet metal fabrication.</h4>
+              <br />
+              <h4>This has helped me developed a keen eye for detail and design, as well as organization and calm under pressure.</h4>
+          </div>
+      </div>
+      <div className="row aboutstuff">
+        <div className="six columns aboutinfo">
+          <h4>I have </h4>
+        </div>
+        <img className="six columns aboutimg" src="https://i.imgur.com/FkRJ6nf.png" alt="" />
+      </div>
+  </div>
+    )
+  } 
+  
+  export default About
