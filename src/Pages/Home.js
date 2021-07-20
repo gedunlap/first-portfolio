eb6e815afe992a4833d5c6c670ef466f13bbf9dd
gedunlap <g.e.dunlap90@gmail.com>
2021-07-20 10:34:52 -0400

    skill icons

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,3 +1,11 @@
+import { FaHtml5, FaCss3Alt, FaReact, FaNodeJs, FaGitAlt, FaGithub } from "react-icons/fa";
+import { IoLogoJavascript } from "react-icons/io";
+import { DiMongodb, DiUnitySmall } from "react-icons/di";
+import { SiRails, SiPostgresql, SiCsharp } from "react-icons/si";
+
+
+
+
 function Home(props){
     return (
         <div className="container home">
@@ -10,10 +18,56 @@ function Home(props){
             </div>
             <div className="twelve columns skills"><h2>Skills</h2></div>
             <div className="row iconrow">
-
+                <div className="two columns skillicon">
+                    <FaHtml5 />
+                    <p>HTML</p>
+                </div>
+                <div className="two columns skillicon">
+                    <FaCss3Alt />
+                    <p>CSS</p>
+                </div>
+                <div className="two columns skillicon">
+                    <IoLogoJavascript />
+                    <p>Javascript</p>
+                </div>
+                <div className="two columns skillicon">
+                    <FaReact />
+                    <p>React</p>
+                </div>
+                <div className="two columns skillicon">
+                    <SiCsharp />
+                    <p>C #</p>
+                </div>
+                <div className="two columns skillicon">
+                    <FaNodeJs />
+                    <p>Express</p>
+                </div>
             </div>
             <div className="row iconrow">
-
+                <div className="two columns skillicon">
+                    <SiRails />
+                    <p>RoR</p>
+                </div>
+                <div className="two columns skillicon">
+                    <DiMongodb />
+                    <p>MongoDb</p>
+                </div>
+                <div className="two columns skillicon">
+                    <SiPostgresql />
+                    <p>Postgresql</p>
+                </div>
+                <div className="two columns skillicon">
+                    <FaGitAlt />
+                    <p>Git</p>
+                </div>
+                <div className="two columns skillicon">
+                    <FaGithub />
+                    <p>Github</p>
+                </div>
+                <div className="two columns skillicon">
+                    <DiUnitySmall />
+                    <p>Unity</p>
+                </div>
             </div>
         </div>
     )
